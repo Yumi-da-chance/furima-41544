3b9f2877ca956da6ad30fceb38ba2a96a0ae9711
Yumi-da-chance <yumi.pruneda@gmail.com>
2025-02-01 17:52:26 +0900

    モデルにてバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :email, presence: true,uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+         validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*[0-9])/ }
+         validates :password_confirmation, presence: true
+         validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+         validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :birth_date , presence: true
+ 
+         has_many :items
+         has_many :records
+
 end
