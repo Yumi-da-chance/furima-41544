ad0cd0b690d65b5ab1c03641668ce96d320e01bf
Yumi-da-chance <yumi.pruneda@gmail.com>
2025-02-03 00:23:05 +0900

    item modelとitemsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_01_075637) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_02_151845) do
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
-    t.string "name"
-    t.string "text"
-    t.text "image"
+    t.string "item_name", null: false
+    t.text "item_information", null: false
+    t.bigint "user_id", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_cost_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "shipping_day_id", null: false
+    t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
@@ -37,4 +44,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_01_075637) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
