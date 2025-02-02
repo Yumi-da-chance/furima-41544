15cdea3c471bdf5acac059987a888ee11fe0c509
Yumi-da-chance <yumi.pruneda@gmail.com>
2025-02-03 02:49:49 +0900

    商品出品ページを表示できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,18 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+
   def index
     @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
   end
 
   private
