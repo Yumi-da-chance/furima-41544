893cd51203db7fcab3efb1f5c64e04ff7d37f9a7
Yumi-da-chance <yumi.pruneda@gmail.com>
2025-02-03 00:59:28 +0900

    ActiveStorageを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,11 @@ class ItemsController < ApplicationController
   def index
     # 必要に応じてロジックを追加
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
 
+  has_many :items
+  has_many :records
+
 end
