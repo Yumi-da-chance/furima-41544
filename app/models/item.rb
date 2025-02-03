f743c19bc20d1a410b87a6cecfc39a05c491b666
Yumi-da-chance <yumi.pruneda@gmail.com>
2025-02-03 16:56:11 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,11 @@
 class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :category
   belongs_to :condition
   belongs_to :shipping_cost
+  belongs_to :prececture
+  belongs_to :shipping_day
 
   
 
@@ -24,8 +26,10 @@ class Item < ApplicationRecord
 
 
 
-  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
--- db/migrate/20250202151845_create_items.rb
+++ db/migrate/20250202151845_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[7.1]
       t.string      :item_name,               null: false
       t.text        :item_information,        null: false
       t.references  :user,                    null: false, foreign_key: true
-      t.integer     :category_id,             null: false
+      t.integer     :category_id,                null: false
       t.integer     :condition_id,            null: false
       t.integer     :shipping_cost_id,        null: false
       t.integer     :prefecture_id,           null: false
