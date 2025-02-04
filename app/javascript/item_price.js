84eab8167d053ba099056eb9061da7f83520718d
Yumi-da-chance <yumi.pruneda@gmail.com>
2025-02-04 20:55:08 +0900

    HTMLをJavaScriptで作成し、ブラウザ上に描画

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,6 +2,8 @@ window.addEventListener('turbo:load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1) 
     console.log(inputValue);
-})
+  });
 });
