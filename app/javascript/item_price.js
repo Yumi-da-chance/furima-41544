c06367444d0cb54687bbcc980826ba1bc25f33ae
Yumi-da-chance <yumi.pruneda@gmail.com>
2025-02-04 21:16:25 +0900

    renderメソッドに対応したイベントを追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,22 @@
-window.addEventListener('turbo:load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1) 
-    console.log(inputValue);
-  });
-});
+  if (priceInput) {
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      const profitDom = document.getElementById("profit");
+
+      const tax = Math.floor(inputValue * 0.1);
+      const profit = inputValue - tax;
+
+      addTaxDom.innerHTML = tax;
+      profitDom.innerHTML = profit;
+
+      console.log("Item price:", inputValue);
+      console.log("Tax:", tax);
+      console.log("Profit:", profit);
+    });
+  }
+};
+ window.addEventListener("turbo:load", price);
+ window.addEventListener("turbo:render", price);
